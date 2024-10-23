70aa8d3384bfc96eae6933ef28d943d9fd73f1df
Glen <house5775@gmail.com>
2024-10-23 20:41:04 +0800

    修改修改video 高度 新增輪播

--- js/product-carousel.js
+++ js/product-carousel.js
@@ -4,19 +4,37 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper(".mySwiper", {
-  spaceBetween: 60,
+  slidesPerView: "auto",
+  speed: 400,
+  spaceBetween: 16,
     effect: "coverflow",
     grabCursor: true,
-    centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
       rotate: 0,
       stretch: 0,
-      depth: 100,
-      modifier: 1,
+      depth: 0,
+      modifier: 0,
       slideShadows: true,
+    },
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
     },
     pagination: {
       el: ".swiper-pagination-featured",
     },
+    breakpoints: {
+      767: {
+        spaceBetween: 80,
+        centeredSlides: true,
+        coverflowEffect : {
+          rotate: 0,
+          stretch: 0,
+          depth: 80,
+          modifier: 1,
+          slideShadows: false,
+        }
+      }
+    }
   });
